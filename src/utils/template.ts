666891d3b6bfba47f5db07301fa255213bba755f
kreison <alankainazovr@gmail.com>
2023-07-09 13:26:54 +0600

    add eslint and update packages

--- src/utils/template.ts
+++ src/utils/template.ts
@@ -1,9 +1,10 @@
 import * as ejs from 'ejs';
 
+
 export interface TemplateData {
     projectName: string
 }
 
-export function render(content: string, data: TemplateData) {
-    return ejs.render(content, data);
+export function render(content: string, data: TemplateData): string {
+  return ejs.render(content, data);
 }
